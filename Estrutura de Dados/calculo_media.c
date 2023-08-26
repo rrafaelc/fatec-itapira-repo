22ff052c91e4157b4fc624c89e20000a1e406b03
Rafael Costa <rafaelcosta8020@gmail.com>
2023-08-26 10:51:37 -0300

    if else if

--- Estrutura de Dados/calculo_media.c
+++ Estrutura de Dados/calculo_media.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include <locale.h>
 
 int main()
 {
-  float nota1, nota2, nota3, media, frequencia;
+  setlocale(LC_ALL, "");
+  float nota1, nota2, media, frequencia;
 
   printf("Entre com a nota 1: ");
   scanf("%f", &nota1);
@@ -10,15 +12,23 @@ int main()
   printf("Entre com a nota 2: ");
   scanf("%f", &nota2);
 
-  printf("Entre com a frequencia: ");
+  printf("Entre com a frequência: ");
   scanf("%f", &frequencia);
 
   media = (nota1 + nota2) / 2;
 
-  printf("Sua media eh: %.1f", media);
+  printf("Sua média foi: %.1f e sua frequência %.1f", media, frequencia);
 
   if (media >= 6 && frequencia >= 75)
     printf(" - Aprovado(a)\n");
+  else if (media < 6 && frequencia >= 75)
+  {
+    printf(" - Reprovado(a) por Média!\n");
+  }
+  else if (media >= 6 && frequencia < 75)
+  {
+    printf(" - Reprovado(a) por Frequência\n");
+  }
   else
-    printf(" - Reprovado(a)\n");
+    printf(" - Reprovado(a) por Média e Frequência\n");
 }
