c434df8940521770357e0e4de2e6fe5e75e9ff69
Rafael Costa <rafaelcosta8020@gmail.com>
2023-08-27 12:26:18 -0300

    Exercicio 1 Java - Aula 03

---
+++ Tecnica de Programacao/ProjetoAcampamento/src/AcampamentoTeste.java
@@ -0,0 +1,11 @@
+public class AcampamentoTeste {
+    public static void main(String[] args) {
+        Acampamento membro_da_classe = new Acampamento();
+
+        membro_da_classe.nome = "João";
+        membro_da_classe.idade = 1;
+        membro_da_classe.imprimir();
+        membro_da_classe.separarGrupo();
+        membro_da_classe.imprimir();
+    }
+}
