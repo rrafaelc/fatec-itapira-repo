c5a27e1f3b08ee33c8be6e499b7a5b414255d100
Rafael Costa <rafaelcosta8020@gmail.com>
2023-09-02 11:32:52 -0300

    Exercicio IMC Aula 03 Tecnicas de programacao

---
+++ Estrutura de Dados/aula03/imc.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+  setlocale(LC_ALL, "");
+  float peso, altura, imc;
+
+  printf("Digite seu peso: ");
+  scanf("%f", &peso);
+
+  printf("Digite sua altura: ");
+  scanf("%f", &altura);
+
+  imc = peso / (altura * altura);
+
+  printf("Seu imc é %f", imc);
+
+  if (imc <= 18.5)
+  {
+    printf("\nPaciente abaixo do peso");
+  }
+  else if (imc >= 18.6 && imc <= 24.9)
+  {
+    printf("\nPaciente no peso ideal");
+  }
+  else if (imc >= 25 && imc <= 29.9)
+  {
+    printf("\nPaciente levemente acima do peso");
+  }
+  else if (imc >= 30 && imc <= 34.9)
+  {
+    printf("\nObesidade Grau I");
+  }
+  else if (imc >= 35 && imc <= 39.9)
+  {
+    printf("\nObesidade Grau II");
+  }
+  else
+  {
+    printf("\nObesidade Grau III");
+  }
+
+  printf("\n");
+}
