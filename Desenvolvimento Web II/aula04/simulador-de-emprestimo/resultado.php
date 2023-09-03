d1d69ae5d5436ceb60b6907f79fb471cd6384678
Rafael Costa <rafaelcosta8020@gmail.com>
2023-09-03 17:14:55 -0300

    Adicionado estilos

--- Desenvolvimento Web II/aula04/simulador-de-emprestimo/resultado.php
+++ Desenvolvimento Web II/aula04/simulador-de-emprestimo/resultado.php
@@ -17,9 +17,6 @@ try {
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="preconnect" href="https://fonts.googleapis.com" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet" />
   <link rel="stylesheet" href="style.css" />
   <title>Resultado da simulação</title>
 </head>
@@ -39,15 +36,17 @@ try {
         Quantidade de parcelas: <?= $parcelas ?>
       </p>
       <p class="juros">
-        Taxa de juros: <?= $juros ?>%
+        Taxa de juros: <?= $juros ?> %
       </p>
       <p>
         Custo Efetivo Total: R$ <?= number_format($valor, 2, ',', '.') ?>
       </p>
     </div>
 
-    <p>Professor Júnior Gonçalves</p>
+    <p class="professor">Professor Júnior Gonçalves</p>
   </div>
+
+  <?= "<a href='./'>Voltar ao MyBank</a>" ?>
 </body>
 
 </html>
