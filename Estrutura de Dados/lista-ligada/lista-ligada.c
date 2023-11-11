a8556f45aa0eb7f583df690a36fa8c7c92eadc10
Rafael Costa <rafaelcosta8020@gmail.com>
2023-11-11 10:52:44 -0300

    lista ligada insercao

--- Estrutura de Dados/lista-ligada/lista-ligada.c
+++ Estrutura de Dados/lista-ligada/lista-ligada.c
@@ -8,10 +8,10 @@ struct lista
 };
 struct lista *aux, *inicio = NULL, *final = NULL;
 
-void insere()
+void insereFinal()
 {
   int x;
-  printf("Entre com um valor: ");
+  printf("\nEntre com um valor: ");
   scanf("%i", &x);
   aux = (struct lista *)malloc(sizeof(struct lista));
   aux->valor = x;
@@ -24,5 +24,60 @@ void insere()
   {
     final->prox = aux;
     final = aux;
+  }
+}
+
+int removeFinal()
+{
+  return 0;
+}
+
+void imprimir()
+{
+  if (inicio == NULL)
+  {
+    printf("\nLista Vazia\n");
+  }
+  else
+  {
+    aux = inicio;
+    while (aux != (struct lista *)NULL)
+    {
+      printf("\nValor da Lista: %i", aux->valor);
+      aux = aux->prox;
+    }
+  }
+
+  printf("\n");
+}
+
+int main()
+{
+  int op;
+  while (op != 4)
+  {
+    printf("\nLista Dinâmica");
+    printf("\nDigite 1 para Inserir");
+    printf("\nDigite 2 para Remover");
+    printf("\nDigite 2 para Imprimir");
+    printf("\nDigite 4 para Sair");
+    printf("\nEntre com a opção: ");
+    scanf("%i", &op);
+
+    switch (op)
+    {
+    case 1:
+      insereFinal();
+      break;
+    case 2:
+      removeFinal();
+      break;
+    case 3:
+      imprimir();
+      break;
+    case 4:
+      exit(1);
+      break;
+    }
   }
 }
